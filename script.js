558da1f1ca1f2d68457cf8c5530ca95daf310d5b
chiragsharma21 <75730835+chiragsharma21@users.noreply.github.com>
2023-05-25 22:47:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,48 +1,98 @@
 const conversionRates = {
     length: {
-        meter: {
-            kilometer: 0.001,
-            mile: 0.000621371
-        },
-        kilometer: {
-            meter: 1000,
-            mile: 0.621371
-        },
-        mile: {
-            meter: 1609.34,
-            kilometer: 1.60934
-        }
+      meter: {
+        kilometer: 0.001,
+        mile: 0.000621371,
+      },
+      kilometer: {
+        meter: 1000,
+        mile: 0.621371,
+      },
+      mile: {
+        meter: 1609.34,
+        kilometer: 1.60934,
+      },
     },
     time: {
-        second: {
-            minute: 1 / 60,
-            hour: 1 / 3600
-        },
-        minute: {
-            second: 60,
-            hour: 1 / 60
-        },
-        hour: {
-            second: 3600,
-            minute: 60
-        }
+      second: {
+        minute: 1 / 60,
+        hour: 1 / 3600,
+      },
+      minute: {
+        second: 60,
+        hour: 1 / 60,
+      },
+      hour: {
+        second: 3600,
+        minute: 60,
+      },
     },
     weight: {
-        gram: {
-            kilogram: 0.001,
-            pound: 0.00220462
-        },
-        kilogram: {
-            gram: 1000,
-            pound: 2.20462
-        },
-        pound: {
-            gram: 453.592,
-            kilogram: 0.453592
-        }
-    }
-};
-
+      gram: {
+        kilogram: 0.001,
+        pound: 0.00220462,
+      },
+      kilogram: {
+        gram: 1000,
+        pound: 2.20462,
+      },
+      pound: {
+        gram: 453.592,
+        kilogram: 0.453592,
+      },
+    },
+    volume: {
+      liter: {
+        milliliter: 1000,
+        gallon: 0.264172,
+      },
+      milliliter: {
+        liter: 0.001,
+        gallon: 0.000264172,
+      },
+      gallon: {
+        liter: 3.78541,
+        milliliter: 3785.41,
+      },
+    },
+    speed: {
+      meterPerSecond: {
+        kilometerPerHour: 3.6,
+        milePerHour: 2.23694,
+      },
+      kilometerPerHour: {
+        meterPerSecond: 0.277778,
+        milePerHour: 0.621371,
+      },
+      milePerHour: {
+        meterPerSecond: 0.44704,
+        kilometerPerHour: 1.60934,
+      },
+    },
+    fuelEconomy: {
+      kilometersPerLiter: {
+        milesPerGallon: 2.35215,
+      },
+      milesPerGallon: {
+        kilometersPerLiter: 0.425144,
+      },
+    },
+    pressure: {
+      pascal: {
+        kilopascal: 0.001,
+        psi: 0.000145038,
+      },
+      kilopascal: {
+        pascal: 1000,
+        psi: 0.145038,
+      },
+      psi: {
+        pascal: 6894.76,
+        kilopascal: 6.89476,
+      },
+    },
+  };
+  
 
 // Category select change event listener
 document.getElementById("categorySelect").addEventListener("change", updateUnitOptions);
